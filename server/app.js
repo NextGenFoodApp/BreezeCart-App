821a7eaeb64e9fd807c097fa8260a59274565408
harithmaduranga <nharithmaduranga@gmail.com>
2024-03-09 21:52:22 +0530

    2024/03/09

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const body_parser = require('body-parser');
+
+const app = express();
+app.use(express.json());
+app.use(body_parser.urlencoded({extended:true}));
+
+const categoryRoutes = require('./routers/categoryRouter');
+const productRoutes = require('./routers/productRouter');
+const shopRoutes = require('./routers/shopRouter');
+const userRoutes = require('./routers/userRouter');
+const orderRoutes = require('./routers/orderRouter');
+
+app.use('/categories', categoryRoutes);
+app.use('/products', productRoutes);
+app.use('/shops', shopRoutes);
+app.use('/users', userRoutes);
+app.use('/orders', orderRoutes);
+
+module.exports = app;
